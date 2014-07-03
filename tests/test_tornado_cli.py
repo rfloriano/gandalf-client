e070cdd111fcb7f32ca07358f5ed463d97b17f58
Rafael Floriano da Silva <rflorianobr@gmail.com>
2014-07-02 21:45:14 -0300

    Python 3 compatibility

--- tests/test_tornado_cli.py
+++ tests/test_tornado_cli.py
@@ -12,6 +12,13 @@ from tests.base import AsyncTestCase
 RAWKEY = "ssh-dss AAAAB3NzaC1kc3MAAACBAIHfSDLpSCfIIVEJ/Is3RFMQhsCi7WZtFQeeyfi+DzVP0NGX4j/rMoQEHgXgNlOKVCJvPk5e00tukSv6iVzJPFcozArvVaoCc5jCoDi5Ef8k3Jil4Q7qNjcoRDDyqjqLcaviJEz5GrtmqAyXEIzJ447BxeEdw3Z7UrIWYcw2YyArAAAAFQD7wiOGZIoxu4XIOoeEe5aToTxN1QAAAIAZNAbJyOnNceGcgRRgBUPfY5ChX+9A29n2MGnyJ/Cxrhuh8d7B0J8UkvEBlfgQICq1UDZbC9q5NQprwD47cGwTjUZ0Z6hGpRmEEZdzsoj9T6vkLiteKH3qLo7IPVx4mV6TTF6PWQbQMUsuxjuDErwS9nhtTM4nkxYSmUbnWb6wfwAAAIB2qm/1J6Jl8bByBaMQ/ptbm4wQCvJ9Ll9u6qtKy18D4ldoXM0E9a1q49swml5CPFGyU+cgPRhEjN5oUr5psdtaY8CHa2WKuyIVH3B8UhNzqkjpdTFSpHs6tGluNVC+SQg1MVwfG2wsZUdkUGyn+6j8ZZarUfpAmbb5qJJpgMFEKQ== f@xikinbook.local"
 
 
+def load_json(json_string):
+    try:
+        return json.loads(json_string)
+    except (ValueError, TypeError):
+        return json.loads(json_string.decode('utf-8'))
+
+
 class TestTornadoGandalfClient(AsyncTestCase):
 
     def setUp(self, *args, **kwargs):
@@ -40,7 +47,7 @@ class TestTornadoGandalfClient(AsyncTestCase):
         response = yield self.gandalf.user_get_keys(user)
         expect(response.code).to_equal(200)
 
-        data = json.loads(response.body)
+        data = load_json(response.body)
         expect(data['foo']).to_equal(RAWKEY)
 
         response = yield self.gandalf.user_delete_key(user, 'foo')
