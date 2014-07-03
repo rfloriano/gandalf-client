74afd82a61e59147dfb869467b50c52011edbcc2
Rafael Floriano da Silva <rflorianobr@gmail.com>
2014-07-02 21:10:52 -0300

    Getting request method by param in GandalfClient

--- gandalf/client.py
+++ gandalf/client.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import requests
-
 try:
     import ujson as json
 except ImportError:
@@ -10,11 +8,10 @@ except ImportError:
 
 class GandalfClient(object):
 
-    def __init__(self, host, port, request=None):
+    def __init__(self, host, port, client):
         self.host = host
         self.port = port
-        self.request = request or self._request_method
-
+        self.client = client
         self.gandalf_server = self._get_gandalf_server()
 
     def _get_gandalf_server(self):
@@ -24,17 +21,12 @@ class GandalfClient(object):
         return '{0}/{1}'.format(self.gandalf_server, route.lstrip('/'))
 
     def _request(self, *args, **kwargs):
-        route = kwargs.pop('url')
-        kwargs['url'] = self._get_url(route)
-        return self.request(*args, **kwargs)
-
-    def _request_method(self, *args, **kwargs):
-        return requests.request(*args, **kwargs)
+        return self.client(*args, **kwargs)
 
     def repository_new(self, name, users, ispublic=False):
         # router.Post("/repository", http.HandlerFunc(api.NewRepository))
         return self._request(
-            url='/repository',
+            url=self._get_url('/repository'),
             method="POST",
             data=json.dumps({'name': name, 'users': users, 'ispublic': ispublic})
         )
@@ -42,14 +34,14 @@ class GandalfClient(object):
     def repository_get(self, name):
         # router.Get("/repository/:name", http.HandlerFunc(api.GetRepository))
         return self._request(
-            url='/repository/{0}'.format(name),
+            url=self._get_url('/repository/{0}'.format(name)),
             method="GET",
         )
 
     def repository_rename(self, old_name, new_name):
         # router.Put("/repository/:name", http.HandlerFunc(api.RenameRepository))
         return self._request(
-            url='/repository/{0}'.format(old_name),
+            url=self._get_url('/repository/{0}'.format(old_name)),
             method="PUT",
             data=json.dumps({'name': new_name})
         )
@@ -57,7 +49,7 @@ class GandalfClient(object):
     def repository_grant(self, users, repositories):
         # router.Post("/repository/grant", http.HandlerFunc(api.GrantAccess))
         return self._request(
-            url='/repository/grant',
+            url=self._get_url('/repository/grant'),
             method="POST",
             data=json.dumps({'users': users, 'repositories': repositories})
         )
@@ -65,7 +57,7 @@ class GandalfClient(object):
     def repository_revoke(self, users, repositories):
         # router.Del("/repository/revoke", http.HandlerFunc(api.RevokeAccess))
         return self._request(
-            url='/repository/revoke',
+            url=self._get_url('/repository/revoke'),
             method="DELETE",
             data=json.dumps({'users': users, 'repositories': repositories})
         )
@@ -73,28 +65,28 @@ class GandalfClient(object):
     def repository_archive(self, name, ref, format):
         # router.Get("/repository/:name/archive/:ref.:format", http.HandlerFunc(api.GetArchive))
         return self._request(
-            url='/repository/{0}/archive/{1}.{2}'.format(name, ref, format),
+            url=self._get_url('/repository/{0}/archive/{1}.{2}'.format(name, ref, format)),
             method="GET",
         )
 
     def repository_contents(self, name, path):
         # router.Get("/repository/:name/contents/:path", http.HandlerFunc(api.GetFileContents))
         return self._request(
-            url='/repository/{0}/contents/{1}'.format(name, path),
+            url=self._get_url('/repository/{0}/contents/{1}'.format(name, path)),
             method="GET",
         )
 
     def repository_delete(self, name):
         # router.Del("/repository/:name", http.HandlerFunc(api.RemoveRepository))
         return self._request(
-            url='/repository/{0}'.format(name.strip('/')),
+            url=self._get_url('/repository/{0}'.format(name.strip('/'))),
             method="DELETE",
         )
 
     def user_add_key(self, name, keys):
         # router.Post("/user/:name/key", http.HandlerFunc(api.AddKey))
         return self._request(
-            url='/user/{0}/key'.format(name),
+            url=self._get_url('/user/{0}/key'.format(name)),
             method="POST",
             data=json.dumps(keys)
         )
@@ -102,21 +94,21 @@ class GandalfClient(object):
     def user_get_keys(self, name):
         # router.Get("/user/:name/keys", http.HandlerFunc(api.ListKeys))
         return self._request(
-            url='/user/{0}/keys'.format(name),
+            url=self._get_url('/user/{0}/keys'.format(name)),
             method="GET",
         )
 
     def user_delete_key(self, name, keyname):
         # router.Del("/user/:name/key/:keyname", http.HandlerFunc(api.RemoveKey))
         return self._request(
-            url='/user/{0}/key/{1}'.format(name, keyname),
+            url=self._get_url('/user/{0}/key/{1}'.format(name, keyname)),
             method="DELETE",
         )
 
     def user_new(self, name, keys):
         # router.Post("/user", http.HandlerFunc(api.NewUser))
         return self._request(
-            url='/user',
+            url=self._get_url('/user'),
             method="POST",
             data=json.dumps({'name': name, 'keys': keys})
         )
@@ -124,14 +116,14 @@ class GandalfClient(object):
     def user_delete(self, name):
         # router.Del("/user/:name", http.HandlerFunc(api.RemoveUser))
         return self._request(
-            url='/user/{0}'.format(name),
+            url=self._get_url('/user/{0}'.format(name)),
             method="DELETE",
         )
 
     def hook_add(self, name, content):
         # router.Post("/hook/:name", http.HandlerFunc(api.AddHook))
         return self._request(
-            url='/hook/{0}'.format(name),
+            url=self._get_url('/hook/{0}'.format(name)),
             method="POST",
             data=content
         )
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import uuid
+
 from preggy import expect
-
 from tests.base import TestCase
-
 import gandalf.client as client
+import requests
 
 RAWKEY = "ssh-dss AAAAB3NzaC1kc3MAAACBAIHfSDLpSCfIIVEJ/Is3RFMQhsCi7WZtFQeeyfi+DzVP0NGX4j/rMoQEHgXgNlOKVCJvPk5e00tukSv6iVzJPFcozArvVaoCc5jCoDi5Ef8k3Jil4Q7qNjcoRDDyqjqLcaviJEz5GrtmqAyXEIzJ447BxeEdw3Z7UrIWYcw2YyArAAAAFQD7wiOGZIoxu4XIOoeEe5aToTxN1QAAAIAZNAbJyOnNceGcgRRgBUPfY5ChX+9A29n2MGnyJ/Cxrhuh8d7B0J8UkvEBlfgQICq1UDZbC9q5NQprwD47cGwTjUZ0Z6hGpRmEEZdzsoj9T6vkLiteKH3qLo7IPVx4mV6TTF6PWQbQMUsuxjuDErwS9nhtTM4nkxYSmUbnWb6wfwAAAIB2qm/1J6Jl8bByBaMQ/ptbm4wQCvJ9Ll9u6qtKy18D4ldoXM0E9a1q49swml5CPFGyU+cgPRhEjN5oUr5psdtaY8CHa2WKuyIVH3B8UhNzqkjpdTFSpHs6tGluNVC+SQg1MVwfG2wsZUdkUGyn+6j8ZZarUfpAmbb5qJJpgMFEKQ== f@xikinbook.local"
 
@@ -13,54 +14,59 @@ class TestGandalfClient(TestCase):
 
     def setUp(self, *args, **kwargs):
         config = self.get_config()
-        self.gandalf = client.GandalfClient(config['GANDALF_HOST'], config['GANDALF_PORT'])
+        self.gandalf = client.GandalfClient(config['GANDALF_HOST'], config['GANDALF_PORT'], requests.request)
 
     def test_can_manage_users(self):
-        response = self.gandalf.user_new('user-tests', {})
+        user = str(uuid.uuid4())
+        response = self.gandalf.user_new(user, {})
         expect(response.status_code).to_equal(200)
 
-        response = self.gandalf.user_delete('user-tests')
+        response = self.gandalf.user_delete(user)
         expect(response.status_code).to_equal(200)
 
     def test_can_manage_user_keys(self):
-        # TODO: add user direct by gandalf
-        self.gandalf.user_new('u-key-tests', {})
+        user = str(uuid.uuid4())
+        self.gandalf.user_new(user, {})
 
-        response = self.gandalf.user_add_key('u-key-tests', {'foo': RAWKEY})
+        response = self.gandalf.user_add_key(user, {'foo': RAWKEY})
         expect(response.status_code).to_equal(200)
 
-        response = self.gandalf.user_get_keys('u-key-tests')
+        response = self.gandalf.user_get_keys(user)
         expect(response.status_code).to_equal(200)
         json = response.json()
         expect(json['foo']).to_equal(RAWKEY)
 
-        response = self.gandalf.user_delete_key('u-key-tests', 'foo')
+        response = self.gandalf.user_delete_key(user, 'foo')
         expect(response.status_code).to_equal(200)
 
-        self.gandalf.user_delete('u-key-tests')
+        self.gandalf.user_delete(user)
 
     def test_can_manage_repositories(self):
-        # TODO: add user direct by gandalf
-        self.gandalf.user_new('u-repositories-test', {})
-        self.gandalf.user_new('u-repositories-test2', {})
+        user = str(uuid.uuid4())
+        user2 = str(uuid.uuid4())
+        repo = str(uuid.uuid4())
+        repo2 = str(uuid.uuid4())
 
-        response = self.gandalf.repository_new('repository-test', ['u-repositories-test'])
+        self.gandalf.user_new(user, {})
+        self.gandalf.user_new(user2, {})
+
+        response = self.gandalf.repository_new(repo, [user])
         expect(response.status_code).to_equal(200)
 
-        # response = self.gandalf.repository_get('repository-test')
-        # expect(response.status_code).to_equal(200)
-
-        response = self.gandalf.repository_rename('repository-test', 'repo-test')
+        response = self.gandalf.repository_get(repo)
         expect(response.status_code).to_equal(200)
 
-        response = self.gandalf.repository_grant(['u-repositories-test2'], ['repo-test'])
+        response = self.gandalf.repository_rename(repo, repo2)
         expect(response.status_code).to_equal(200)
 
-        response = self.gandalf.repository_revoke(['u-repositories-test2'], ['repo-test'])
+        response = self.gandalf.repository_grant([user2], [repo2])
         expect(response.status_code).to_equal(200)
 
-        response = self.gandalf.repository_delete('repo-test')
+        response = self.gandalf.repository_revoke([user2], [repo2])
         expect(response.status_code).to_equal(200)
 
-        self.gandalf.user_delete('u-repositories-test')
-        self.gandalf.user_delete('u-repositories-test2')
+        response = self.gandalf.repository_delete(repo2)
+        expect(response.status_code).to_equal(200)
+
+        self.gandalf.user_delete(user)
+        self.gandalf.user_delete(user2)
